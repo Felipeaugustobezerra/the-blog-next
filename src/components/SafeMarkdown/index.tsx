8ae4df7e4e98bce5333c015f0891e3b0bfaf0755
Felipeaugustobezerra <58082993+Felipeaugustobezerra@users.noreply.github.com>
2025-11-17 23:03:54 +0000

    Replacing and modifying HTML elements in React Markdown

--- src/components/SafeMarkdown/index.tsx
+++ src/components/SafeMarkdown/index.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import { table } from 'console';
 import ReactMarkdown from 'react-markdown';
 import rehypeSanitize from 'rehype-sanitize';
 import remarkGfm from 'remark-gfm';
@@ -25,6 +26,17 @@ export function SafeMarkdown({ markdown }: SafeMarkdownProps) {
       <ReactMarkdown
         rehypePlugins={[rehypeSanitize]}
         remarkPlugins={[remarkGfm]}
+        components={{
+          table: ({ node, ...props }) => {
+            if (!node?.children) return '';
+
+            return (
+              <div className='overflow-x-auto'>
+                <table className='w-full min-w-[600px] ' {...props} />
+              </div>
+            );
+          },
+        }}
       >
         {markdown}
       </ReactMarkdown>
