98999f63faeca9cfb7c4500375c6bd0318c115f0
Felipeaugustobezerra <58082993+Felipeaugustobezerra@users.noreply.github.com>
2025-11-17 21:32:36 +0000

    Styling SinglePost content with tailwind css

---
+++ src/components/PostDate/index.tsx
@@ -0,0 +1,16 @@
+import { formatDateTime, formatRelativeDate } from '@/utils/format-datetime';
+
+type PostDateProps = {
+  dateTime: string;
+};
+export function PostDate({ dateTime }: PostDateProps) {
+  return (
+    <time
+      className='text-slate-600 block text-sm/tight '
+      dateTime={dateTime}
+      title={formatRelativeDate(dateTime)}
+    >
+      {formatDateTime(dateTime)}
+    </time>
+  );
+}
--- src/components/SinglePost/index.tsx
+++ src/components/SinglePost/index.tsx
@@ -1,4 +1,7 @@
 import { findPostBySlugCached } from '@/lib/post/queries';
+import Image from 'next/image';
+import { PostHeading } from '../PostHeading';
+import { PostDate } from '../PostDate';
 
 type SinglePostProps = {
   slug: string;
@@ -8,8 +11,23 @@ export async function SinglePost({ slug }: SinglePostProps) {
   const post = await findPostBySlugCached(slug);
 
   return (
-    <div>
-      <p>{post.content}</p>
-    </div>
+    <article className='mb-16'>
+      <header className='group flex flex-col gap-4 mb-4'>
+        <Image
+          className='rounded-xl mb-6'
+          src={post.coverImageUrl}
+          width={1024}
+          height={720}
+          alt={post.title}
+        ></Image>
+
+        <PostHeading url={`/post/${post.slug}`}>{post.title}</PostHeading>
+        <p>
+          {post.author} | <PostDate dateTime={post.createdAt} />
+        </p>
+        <p className='mb-6 text-xl'>{post.excerpt}</p>
+        <div>{post.content}</div>
+      </header>
+    </article>
   );
 }
